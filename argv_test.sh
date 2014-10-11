61822cd4e9a9fe06c91933d18110b5ad1dff8b52
wujiang1 <wujiang1@staff.sina.com.cn>
2014-10-11 11:31:51 +0800

    init

---
+++ argv_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $# -eq 1 ]]; then
+    echo $#
+    #echo $1
+fi
+
+if [[ -n $1 ]]; then
+    echo $1
+fi
---
+++ while_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true
+    do
+        echo "a"
+        sleep 3
+        echo "b"
+        break
+    done
