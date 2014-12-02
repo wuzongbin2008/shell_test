abf04578afcbffc4caeb4f69cc506fe74c089f2c
wujiang1 <wujiang1@staff.sina.com.cn>
2014-12-02 12:34:12 +0800

    test

--- ssh_login.sh
+++ ssh_login.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 
 set timeout 30
-spawn ssh wujiang1@202.106.182.212
+spawn ssh aaaa@127.0.01
 expect "password:"
-send "s8O6AjOO+rmpmjOdWfeOtLCgncPTO88QnwUEWyaFnWY/fjyag9pM51vVj+YmNysnE\r"
+send "xxxxx"
 interact
